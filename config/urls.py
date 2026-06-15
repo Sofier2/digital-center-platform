c2d8272c271975778214ae05f7ccc847cf46695f
Sofier2 <sophiakryviak@gmail.com>
2026-06-15 17:05:16 +0300

    Serve static files on Render

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -14,3 +15,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+    ]
