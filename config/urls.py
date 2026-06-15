443f9e1918e9b1daf6499cd63a6b7055ab7441db
Sofier2 <sophiakryviak@gmail.com>
2026-06-15 18:21:15 +0300

    Add platform favicon route

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ from django.views.static import serve
 
 
 urlpatterns = [
+    path("favicon.ico", serve, {"document_root": settings.BASE_DIR / "static" / "img", "path": "logo-chip-cutout.png"}),
     path("admin/", admin.site.urls),
     path("accounts/login/", auth_views.LoginView.as_view(template_name="learning/login.html"), name="login"),
     path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
